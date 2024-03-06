bf4594b30b62ac526a66d586846455c9bc44b55a
Jay9ce <joshuaichipi@gmail.com>
2024-03-06 11:35:39 +0100

    Small Test

--- drafboard/main.c
+++ drafboard/main.c
@@ -7,7 +7,7 @@ int main() {
     char board[size][size];
 
     for(int i = 0; i < size; i++){
-        for(int j = 0; j < size; j++){
+        for(int j = 0; j < size; j++){ 
             if (i % 2 > 0){
                 if (j % 2 > 0){
                     board[i][j] = '*';
